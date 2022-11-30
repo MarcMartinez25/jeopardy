8fdf93ecc8c4a4db8666694ccb2b9b92a127fe69
Marco Martinez <martinezma025@gmail.com>
2022-11-30 01:11:45 -0600

    fixed lint errors

--- components/Header.js
+++ components/Header.js
@@ -1,12 +1,12 @@
-import React from 'react'
-import Head from 'next/head'
+import React from 'react';
+import Head from 'next/head';
 
-export const Header = () => {
-  return (
-    <Head>
-      <title>Jeopardy</title>
-      <meta name="jeopardy game" content="a fun game of jeopardy" />
-      <link rel="icon" href="/favicon.ico" />
-    </Head>
-  )
-}
+const Header = () => (
+  <Head>
+    <title>Jeopardy</title>
+    <meta name="jeopardy game" content="a fun game of jeopardy" />
+    <link rel="icon" href="/favicon.ico" />
+  </Head>
+);
+
+export default Header;
